5d6adfdb607c5a8d7c03e5a16181c6ef8c2e742c
Debbie <asamoahjaeldeborah@gmail.com>
2021-10-02 18:33:23 +0000

    Created the navbar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
+
+
+
+    // <div className="App">
+    //   <Navbar /> 
+    // </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,68 @@
+import React , { useState} from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import { MdFingerprint } from 'react-icons/md';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { LinkR } from 'react-scroll';
+
+
+function Navbar() {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to='/' className="navbar-logo">
+                        Zubi
+                        <MdFingerprint className="navbar-icon" />
+                    </Link>
+
+                    <div className="nav-icon"  onClick={handleClick} >
+                        {click ? <FaTimes /> :<FaBars /> }
+                    </div>
+
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link exact to='/' activeClassName="active" className="nav-links" onClick={handleClick}>
+                                Home
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link exact to='/about' activeClassName="active" className="nav-links" onClick={handleClick}>
+                                About
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link exact to='/services' activeClassName="active" className="nav-links" onClick={handleClick}>
+                                Services
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link exact to='/signup' activeClassName="active" className="nav-links" onClick={handleClick}>
+                                Sign Up
+                            </Link>
+                        </li>
+
+                        <div className="nav-button">
+                        <Link to='/signin' className="nav-btn-link">Sign In</Link>
+                    </div>
+
+                    </ul>
+
+                    
+                </div>
+                
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
